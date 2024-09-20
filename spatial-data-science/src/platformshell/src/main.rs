60246a10871edf611b4cb927c8569a25bc3c4d8a
Jan Tschada <j.tschada@esri.de>
2024-09-20 15:19:16 +0200

    #3 Added a custom error type
    - inspect error results
    - access feature attributes

--- spatial-data-science/src/platformshell/src/main.rs
+++ spatial-data-science/src/platformshell/src/main.rs
@@ -1,7 +1,9 @@
 use geo::Point;
+use platformshell::LocationServicesError;
 use reqwest::Url;
 use serde_esri::features::FeatureSet;
 use serde_json::json;
+use serde_json::Value;
 use std::env;
 use std::io::Read;
 
@@ -62,6 +64,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Query the feature service
     let urban_hri_url = env::var("URBAN_HEAT_RISK_INDEX_FEATURE_SERVICE_URL")?;
+    let out_fields = "HRI, TEMP";
     let query_url = Url::parse_with_params(
         &(urban_hri_url + "/query"),
         &[
@@ -69,7 +72,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ("geometryType", "esriGeometryPoint"),
             ("geometry", &location_str),
             ("inSR", &location_wkid_str),
-            ("outFields", "*"),            
+            ("outFields", &out_fields),
             ("returnGeometry", "true"),
             ("f", "json"),
         ],
@@ -80,19 +83,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read the request into a String
     response.read_to_string(&mut body)?;
 
+    // Parse the response body as JSON
+    let json_body: Value = serde_json::from_str(&body)?;
+
+    // Check if the JSON contains an error
+    if let Some(error) = json_body.get("error") {
+        let code = error.get("code").and_then(Value::as_i64).unwrap_or(0) as i32;
+        let message = error.get("message").and_then(Value::as_str).unwrap_or("Unknown error").to_string();
+        //eprintln!("{}", message);
+        return Err(Box::new(LocationServicesError { code, message }));
+    }
     //println!("{:?}", body);
 
     // Parse into a 2D FeatureSet
     let hri_featureset: FeatureSet<2> = serde_json::from_str(&body)?;
 
-     // Extract and print the JSON representation of the geometries
-     for hri_feature in hri_featureset.features {
+    // Extract and print the JSON representation of the features
+    for hri_feature in hri_featureset.features {
+        if let Some(hri_attributes) = hri_feature.attributes {
+            for (key, value) in hri_attributes.iter() {
+                println!("{}: {}", key, value);
+            }
+        }
         if let Some(hri_geometry) = hri_feature.geometry {
             if let Some(polygon) = hri_geometry.as_polygon() {
                 let json_geometry = json!(polygon);
                 println!("{}", serde_json::to_string_pretty(&json_geometry)?);
             } else {
-                eprintln!("Geometry is not a polygon.");
+                //eprintln!("Geometry is not a polygon.");
+                return Err(Box::new(LocationServicesError { code:9999, message:"Geometry is not a polygon.".to_string() }));
             }
         }
     }
