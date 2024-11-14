d8d622203374a9806981108eb339634b35eeb44e
Alexander Erbe <a.erbe@esri.de>
2024-11-14 17:30:02 +0100

    Added Heat Risk Layers

--- native-apps/urban-heat-notifier/urban_heat_notifier/lib/main.dart
+++ native-apps/urban-heat-notifier/urban_heat_notifier/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:arcgis_maps/arcgis_maps.dart';
+import 'dart:convert';
 import 'env/env.dart';
 
 void main() {
@@ -24,6 +25,9 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   bool _switchValue = false;
   final _mapViewController = ArcGISMapView.createController();
+  late ArcGISVectorTiledLayer _hriVectorTiledLayer;
+  late FeatureLayer _hriFeatureLayer;
+  
 
   @override
   void initState() {
@@ -33,11 +37,14 @@ class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(Env.title)
+      ),
       body: Stack(
         children: [
           ArcGISMapView(
             controllerProvider: () => _mapViewController,
-            onMapViewReady: onMapViewReady
+            onMapViewReady: onMapViewReady            
       ),
       Positioned(
             bottom: 25,
@@ -62,18 +69,63 @@ class _MapScreenState extends State<MapScreen> {
   }
 
     void onMapViewReady() {
-      _mapViewController.arcGISMap = ArcGISMap.withBasemapStyle(BasemapStyle.arcGISLightGray);
+
+      BasemapStyle basemapStyle = BasemapStyle.values.firstWhere((e) => e.toString() == Env.basemap);
+      var map = ArcGISMap.withBasemapStyle(basemapStyle);
+
+
+      final parsedInitialViewPoint = jsonDecode(Env.initialViewPont);
+
+      map.initialViewpoint = Viewpoint.fromCenter(
+      ArcGISPoint(
+        x: double.parse(parsedInitialViewPoint["lon"].toString()),
+        y: double.parse(parsedInitialViewPoint["lat"].toString()),
+        spatialReference: SpatialReference.wgs84,
+      ),
+      scale: double.parse(parsedInitialViewPoint["scale"].toString()),
+    );
+
+
+      var portalItem = PortalItem.withPortalAndItemId(portal: Portal.arcGISOnline(), itemId: Env.hriVectorTileServicePortalItemID);
+      _hriVectorTiledLayer = ArcGISVectorTiledLayer.withItem(portalItem);
+      map.operationalLayers.add(_hriVectorTiledLayer);
+
+      portalItem = PortalItem.withPortalAndItemId(portal: Portal.arcGISOnline(), itemId: Env.hriFeatureServicePortalItemID);
+      _hriFeatureLayer = FeatureLayer.withFeatureLayerItem(portalItem);
+      _hriFeatureLayer.definitionExpression = "hri >= 9";
+      _hriFeatureLayer.opacity = .5;
+      map.operationalLayers.add(_hriFeatureLayer);
+
+      _mapViewController.arcGISMap = map;
 
       // Set the initial system location data source and auto-pan mode.
       _mapViewController.locationDisplay.dataSource = SystemLocationDataSource();
       _mapViewController.locationDisplay.autoPanMode = LocationDisplayAutoPanMode.compassNavigation;
+      _mapViewController.locationDisplay.initialZoomScale = 2000;
+
+      _mapViewController.onScaleChanged.listen((scale){
+        _toggleUserLocation();
+        _toggleLayerVisibility(scale);
+      });
   }
 
-    void _toggleUserLocation() {
+    void _toggleUserLocation() async {
       if (_switchValue) {
+        _mapViewController.locationDisplay.autoPanMode = LocationDisplayAutoPanMode.compassNavigation;
         _mapViewController.locationDisplay.start();
       } else {
+        _mapViewController.locationDisplay.autoPanMode = LocationDisplayAutoPanMode.off;
         _mapViewController.locationDisplay.stop();
+      }
+  }
+
+      void _toggleLayerVisibility(double scale) async {
+      if (scale <= 10000) {
+        _hriVectorTiledLayer.isVisible = false;
+        _hriFeatureLayer.isVisible = true;
+      } else {
+        _hriVectorTiledLayer.isVisible = true;
+        _hriFeatureLayer.isVisible = false;
       }
   }
 }
